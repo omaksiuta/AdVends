8b9b01c0fc4f54d494b42f90937b9a00effec460
oleksandrmaksiuta <Micropsoft!0>
2019-10-12 15:12:15 +0300

    updated sce theme

--- wordpress/wp-content/themes/corporate-plus/acmethemes/customizer/design-options/background-image.php
+++
@@ -1,4 +0,0 @@
-<?php
-/*Adding default background image section in new panel */
-$wp_customize->get_section( 'background_image' )->panel = 'corporate-plus-design-panel';
-$wp_customize->get_section( 'background_image' )->priority = 50;
