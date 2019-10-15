9ca7f322381abebb595490464b88ce4a2fdb598f
oleksandrmaksiuta <Micropsoft!0>
2019-10-16 00:13:33 +0300

    set page template

---
+++ wordpress/wp-content/themes/onepress/constructor-configurations/db_config.php
@@ -0,0 +1,5 @@
+<?php
+
+$mysqli = new mysqli("localhost", "sweet", "sweet", "db2");
+
+?>
--- wordpress/wp-content/themes/onepress/header.php
+++ wordpress/wp-content/themes/onepress/header.php
@@ -12,22 +12,32 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+
+
+    <!--//TODO used i rowswitcher @ https://www.itsolutionstuff.com/post/php-dynamic-drag-and-drop-table-rows-using-jquery-ajaxexample.html-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <!--//TODO used i rowswitcher @ https://www.itsolutionstuff.com/post/php-dynamic-drag-and-drop-table-rows-using-jquery-ajaxexample.html-->
+
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php do_action( 'onepress_before_site_star' ); ?>
+<?php do_action('onepress_before_site_star'); ?>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'onepress' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'onepress'); ?></a>
     <?php
     /**
      * Hooked: onepress_site_header
      *
      * @see onepress_site_header
      */
-    do_action( 'onepress_site_start' );
+    do_action('onepress_site_start');
     ?>
